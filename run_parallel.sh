e83c6facafed100e0a6b572c46b909240c80fb91
ubuntu <gaboandrez99@gmail.com>
2023-04-21 16:09:52 +0000

    Add Results

--- run_parallel.sh
+++ run_parallel.sh
@@ -7,5 +7,4 @@ python -m gprMax sim_3.in -gpu 3 &> gpu3.txt &
 python -m gprMax sim_4.in -gpu 4 &> gpu4.txt &
 python -m gprMax sim_5.in -gpu 5 &> gpu5.txt &
 python -m gprMax sim_6.in -gpu 6 &> gpu6.txt &
-python -m gprMax sim_7.in -gpu 7 &> gpu7.txt &
-wait
+python -m gprMax sim_7.in -gpu 7 &> gpu7.txt 
